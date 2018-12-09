7b8265a39d8fdf91691d9e7dd85519fbe659b0a8
George Betts <gbetts24@gmail.com>
2018-12-09 16:17:45 +0000

    completed day8 part1

--- day8/day8_part1.php
+++ day8/day8_part1.php
@@ -1,24 +1,26 @@
 <?php
-$input = preg_replace('/\s+/', '', file_get_contents('debug_input.txt'));
+$input = file_get_contents('input.txt');
 
 $firstHeader = substr($input, 0, 2);
 $output = 0;
 $nextHeader = "";
 $headers = [];
 $headerIndex = 0;
-$charArray = str_split($input);
+$nextHeaderLen = 0;
+$charArray = explode(" ", $input);
 foreach ($charArray as $char) {
     $charIsMetadata = false;
-    switch (strlen($nextHeader)) {
+    switch ($nextHeaderLen) {
         case 0:
             //Brand new header - add the child node quantity
+            $headerIndex++;
             $headers[$headerIndex][0] = intval($char);
-            $nextHeader .= $char;
+            $nextHeaderLen++;
             break;
         case 1:
             //Header contains the quantity of child nodes - add the quantity of metadata
             $headers[$headerIndex][1] = intval($char);
-            $nextHeader .= $char;
+            $nextHeaderLen++;
             break;
         case 2:
             //Header contains the quantity of metadata entries - save this off, this header is complete
@@ -26,16 +28,41 @@ foreach ($charArray as $char) {
                 $headers[$headerIndex][0]--;
                 $headerIndex++;
                 //The next node is a child node, so begin the next header
-                $nextHeader = "";
+                $nextHeaderLen = 0;
                 $headers[$headerIndex][0] = intval($char);
-                $nextHeader .= $char;
+                $nextHeaderLen++;
             } elseif ($headers[$headerIndex][1] > 0) {
                 //No Child nodes so get the metadata for this node
                 $output += intval($char);
                 $headers[$headerIndex][1]--;
+                if ($headers[$headerIndex][1] == 0) {
+                    //The following while loop checks back through previous headers to see if there are any remaining child nodes or metadata
+                    $remainingChildNodesOrMetaData = false;
+                    while (!$remainingChildNodesOrMetaData) {
+                        if ($headers[$headerIndex][0] == 0) {
+                            if ($headers[$headerIndex][1] != 0) {
+                                //Still metadata left for this header
+                                $nextHeaderLen = 2;
+                                $remainingChildNodesOrMetaData = true;
+                            } else {
+                                if ($headerIndex != 1) {
+                                    //No child nodes or metadata left, check the previous header
+                                    $headerIndex--;
+                                } else {
+                                    //The very last node
+                                    $remainingChildNodesOrMetaData = true;
+                                }
+                            }
+                        } else {
+                            //Still child nodes left, don't go to the previous header yet
+                            $nextHeaderLen = 2;
+                            $remainingChildNodesOrMetaData = true;
+                        }
+                    }
+                }
             } else {
                 //Node is done
-                $nextHeader = "";
+                $nextHeaderLen = 0;
                 $headerIndex--;
             }
             break;
